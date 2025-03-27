ddd8372dcb5d7c0918a60bf575e0a8f2c74ac564
Cemus <24947582+Cemus@users.noreply.github.com>
2025-03-27 16:54:49 +0100

    Random category label

--- cypress/e2e/ex2.cy.js
+++ cypress/e2e/ex2.cy.js
@@ -3,8 +3,8 @@ describe("Exercice 2", () => {
     cy.visit("https://127.0.0.1:8000/category");
   });
 
-  it("Editer la première catégorie", () => {
-    cy.get("a").contains("edit").click();
+  it("Editer dans les règles de l'art une catégorie en ajoutant un label qui n'existe pas :", () => {
+    cy.get("td").find("a").eq(1).contains("edit").click();
 
     let word = "";
     for (let index = 0; index < 25; index++) {
@@ -12,22 +12,26 @@ describe("Exercice 2", () => {
       word += letter;
     }
 
+    cy.get("#category_label").clear();
     cy.get("#category_label").type(word);
     cy.get(".btn").contains("Update").click();
   });
 
-  it("Remplir le formulaire", () => {
-    cy.visit("https://127.0.0.1:8000/category");
+  it("Remplir le formulaire avec un label déjà présent dans la BDD :", () => {
+    cy.get("tbody")
+      .find("tr")
+      .eq(0)
+      .find("td")
+      .eq(1)
+      .invoke("text")
+      .then((word) => {
+        cy.get("td").find("a").eq(3).contains("edit").click();
 
-    cy.get("a").contains("edit").click();
+        cy.get("#category_label").clear();
+        cy.get("#category_label").type(word);
+        cy.get(".btn").contains("Update").click();
 
-    const word = "azdazdazd";
-
-    cy.get("#category_label").clear();
-
-    cy.get("#category_label").type(word);
-    cy.get(".btn").contains("Update").click();
-
-    cy.get("title").should("contain", "500 Internal Server Error");
+        cy.get("title").should("contain", "500 Internal Server Error");
+      });
   });
 });
