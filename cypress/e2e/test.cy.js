3e90cebe9c1cfacd21c7af98946fa130fb3f1799
Cemus <24947582+Cemus@users.noreply.github.com>
2025-03-27 14:25:15 +0100

    Add another test

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -7,15 +7,26 @@ describe("Test Cypress e2e", () => {
     cy.get(".btn").click();
 
     cy.visit("https://127.0.0.1:8000/category");
-    cy.get("td").eq(2).contains("new").should("be.visible");
+    cy.get("td").contains("new").should("be.visible");
   });
 
-  it("Simulation erreur", () => {
+  it("Simulation d'erreur : ne change pas de page", () => {
     cy.visit("https://127.0.0.1:8000/category/new");
 
     cy.get(".btn").click();
 
-    cy.visit("https://127.0.0.1:8000/category");
-    cy.get("td").contains("erreur").should("not.exist");
+    cy.location().should((location) => {
+      expect(location.href).to.eq("https://127.0.0.1:8000/category/new");
+    });
+  });
+
+  it("Deuxième simulation erreur : label unique", () => {
+    cy.visit("https://127.0.0.1:8000/category/new");
+
+    cy.get("#category_label").type("new");
+
+    cy.get(".btn").click();
+
+    cy.get("title").should("contain", "500 Internal Server Error");
   });
 });
