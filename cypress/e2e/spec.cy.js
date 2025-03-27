1d97022e2c292fb02890a85344e54b8e0ff54250
Cemus <24947582+Cemus@users.noreply.github.com>
2025-03-27 13:43:06 +0100

    Add tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,23 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("Test Cypress e2e", () => {
+  it("Ajoute une catégorie 'new'", () => {
+    cy.visit("https://127.0.0.1:8000/category/new");
+
+    cy.get("#category_label").type("new");
+
+    cy.get(".btn").click();
+
+    cy.visit("https://127.0.0.1:8000/category");
+    cy.get("td").contains("new").should("be.visible");
+  });
+
+  it("Simulation erreur", () => {
+    cy.visit("https://127.0.0.1:8000/category/new");
+
+    cy.get("#category_label").type("nouvelle_categorie");
+
+    cy.get(".btn").click();
+
+    cy.visit("https://127.0.0.1:800/category");
+    cy.get("td").contains("new").should("not.exist");
+  });
+});
